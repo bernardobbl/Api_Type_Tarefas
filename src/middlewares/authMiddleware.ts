ff3937595b1d3b0ac0efafc907a6fb22aaa48551
Bernardo Barcellos Leite <bernardobarcellosleite@MacBook-Air-de-Bernardo.local>
2025-11-10 12:48:56 -0300

    O ESLint agora ignora a regra @typescript-eslint/no-namespace apenas para essa linha específica O código mantém o padrão recomendado do TypeScript/Express para estender tipos globais Não há mais erros de lint no arquivo

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
+// eslint-disable-next-line @typescript-eslint/no-namespace
 declare global {
     namespace Express {
         interface Request {
