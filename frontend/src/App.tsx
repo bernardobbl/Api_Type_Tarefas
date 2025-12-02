c2ba163aea97d3e23694fd987e535ba99ff585a4
João Victor <jvictoraraujo2004@gmail.com>
2025-12-01 21:51:15 -0300

    chore: configura ambiente Docker e PostgreSQL para desenvolvimento local

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -30,4 +30,27 @@ function App() {
 }
 
 export default App;
+ 
 
+ // Sem login temporariamente 
+/* 
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Tasks from './pages/Tasks';
+import Categories from './pages/Categories';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/tasks" replace />} />
+          <Route path="tasks" element={<Tasks />} />
+          <Route path="categories" element={<Categories />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App; */
