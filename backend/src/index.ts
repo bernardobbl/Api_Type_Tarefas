1c544ff35cb7ea6277e8e93b250aa845e7daaf8a
brunotan14 <brunotan63@gmail.com>
2025-12-03 22:10:09 -0300

    bruno

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,6 @@ import path from 'path';
 // Procura o .env na raiz do projeto (um nível acima da pasta backend)
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
-import cors from "cors";
 import express from "express";
 import router from "./routes/routes";
 import { setupSwagger } from './swagger';
@@ -12,16 +11,16 @@ import { setupSwagger } from './swagger';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:5173";
-
-app.use(
-  cors({
-    origin: CORS_ORIGIN,
-    credentials: true,
-  })
-);
-
-
+// Middleware para CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 // Middleware para interpretar JSON
 app.use(express.json());
